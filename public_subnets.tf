f4a7d29b2a32e6e69cb4bc9a4f059bf49dd9f41e
Junior Salvador <junioronliner@gmail.com>
2024-10-10 09:52:07 -0300

    feat.: improve README docs with terraform-docs :rocket:

--- public_subnets.tf
+++ public_subnets.tf
@@ -33,23 +33,23 @@ resource "aws_route_table" "public_internet_access" {
 }
 
 resource "aws_route" "public_access" {
-  route_table_id = aws_route_table.public_internet_access.id
+  route_table_id         = aws_route_table.public_internet_access.id
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.gw.id
-  
+  gateway_id             = aws_internet_gateway.gw.id
+
 }
 
 resource "aws_route_table_association" "public_1a" {
-  subnet_id = aws_subnet.public_subnet_1a.id
-  route_table_id = aws_route_table.public_internet_access.id  
+  subnet_id      = aws_subnet.public_subnet_1a.id
+  route_table_id = aws_route_table.public_internet_access.id
 }
 
 resource "aws_route_table_association" "public_1b" {
-  subnet_id = aws_subnet.public_subnet_1b.id
-  route_table_id = aws_route_table.public_internet_access.id  
+  subnet_id      = aws_subnet.public_subnet_1b.id
+  route_table_id = aws_route_table.public_internet_access.id
 }
 
 resource "aws_route_table_association" "public_1c" {
-  subnet_id = aws_subnet.public_subnet_1c.id
-  route_table_id = aws_route_table.public_internet_access.id  
+  subnet_id      = aws_subnet.public_subnet_1c.id
+  route_table_id = aws_route_table.public_internet_access.id
 }
